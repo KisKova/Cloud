c809e12d6df4e1c86fe475c3a878b4e3cb157a55
umar <138657210+UmarSalem@users.noreply.github.com>
2023-11-14 20:18:59 +0100

    update

--- SmartHomeAPI/SmartHomeController/Program.cs
+++ SmartHomeAPI/SmartHomeController/Program.cs
@@ -1,3 +1,14 @@
+﻿namespace SmartHomeController;
+
+using Contracts;
+using EFCData;
+using FirebaseAdmin;
+using MyNotificationService;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WsListenerBackgroundService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,21 +16,86 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Adds api key authorization in Swagger UI 
+
+// Requires a header with a key-value pair of "ApiKey":"{value}"
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Smart Home System Cloud part API", Version = "v1" });
+    
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "ApiKey must appear in header",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "ApiKey",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { key, new List<string>() }
+    };
+    
+    c.AddSecurityRequirement(requirement);
+});
+
+if (FirebaseApp.DefaultInstance == null) {
+    var defaultApp = FirebaseApp.Create(new AppOptions() {
+        Credential = GoogleCredential
+            .FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.json")),
+    });
+
+    Console.WriteLine(defaultApp.Name);
+}
+
+//adding all services
+
+builder.Services.AddDbContext<GreenhouseSystemContext>();
+builder.Services.AddScoped<IMeasurementService, MeasurementDao>();
+builder.Services.AddScoped<IUserService, UserDao>();
+builder.Services.AddScoped<IGreenHouseService, GreenHouseDao>();
+builder.Services.AddScoped<IPlantProfileService, PlantProfileDao>();
+builder.Services.AddScoped<IThresholdService, ThresholdDao>();
+builder.Services.AddScoped<INotificationClient, NotificationClient>();
+
+//Ws-client
+builder.Services.AddHostedService<BackgroundListener>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+
+//websockets:
+// https://learn.microsoft.com/en-us/aspnet/core/fundamentals/websockets?view=aspnetcore-7.0
+// this is how keepalive interval can be set (default is 2mins)
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+//specify allowed request origins
+// webSocketOptions.AllowedOrigins.Add("https://client.com");
+app.UseWebSockets(webSocketOptions);
+
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
