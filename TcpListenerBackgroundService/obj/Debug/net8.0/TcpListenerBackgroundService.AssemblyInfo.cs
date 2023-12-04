b40e6e9f53875a788a61396ec97423b8acb127ca
marcellkovacs <kovacsmarci927@gmail.com>
2023-12-04 09:52:01 +0100

    .gitignore add bin and obj folder

--- TcpListenerBackgroundService/obj/Debug/net8.0/TcpListenerBackgroundService.AssemblyInfo.cs
+++ TcpListenerBackgroundService/obj/Debug/net8.0/TcpListenerBackgroundService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TcpListenerBackgroundService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b7ec56a0bd9048de6a4df27d629f2dba30c5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TcpListenerBackgroundService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TcpListenerBackgroundService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
