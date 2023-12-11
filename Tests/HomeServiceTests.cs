4a095cf9cc8357e878b9f5d3cdfec095b5b08771
marcellkovacs <kovacsmarci927@gmail.com>
2023-12-11 14:40:37 +0100

    Fixed NUnit test.

--- Tests/HomeServiceTests.cs
+++ Tests/HomeServiceTests.cs
@@ -36,11 +36,9 @@ public class HomeServiceTests
         //var sensorData = new SensorData(100, 99, "SHSDIG9999");
 
         //sensorData.HomeId = 1;
-       ;
+       
 
         Console.WriteLine(_homeDao.RetrieveAllHomeIdsFromDB());
-
-        Assert.Equals(true, false);
 
         // Act
         //var addedHome = await _homeDao.AddNewHome(user.Id, home);
